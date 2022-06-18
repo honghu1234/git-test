1c55e00f6b7102370be2471fb1887d81b4278d01
root <2844598296@qq.com>
2022-06-18 10:21:42 +0800

    pushgithup2 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello linux");
         System.out.println("hello gitee");
          System.out.println("hello githup");
-
+        System.out.println("hello githup2");
     }
 }
