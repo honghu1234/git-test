33062558a7f750ceeba10a37fec2f720917f5949
honghu1234 <107667734+honghu1234@users.noreply.github.com>
2022-06-18 09:36:52 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello hello");
         System.out.println("hello gitee");
          System.out.println("hello githup");
-                 System.out.println("githup改动");
+                 System.out.println("githup拉取");
     }
 }
